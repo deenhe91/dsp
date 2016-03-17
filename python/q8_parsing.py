082b65b0aef63a36734cd8dd8716cd70b9edd9d0
deenhe91 <hannah.deen91@gmail.com>
2016-03-17 16:05:37 +0100

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -9,11 +9,53 @@
 
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+import pandas as pd
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+import numpy as np
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+def read_data(data):
+  	file_obj = open(data)
+  	csv_file = pd.read_csv(file_obj)
+  	return csv_file
+
+football = read_data('football.csv')
+football = football.set_index('Team')
+
+print(football)
+
+# df.set_index('head0') to reassign a column to row names
+# i.e. football = football.set_index('Team')
+  		
+
+#col1 and col2 must be ints..
+
+def get_min_score_difference(parsed_data, col1, col2):
+    goal_dict = {}
+    for row in range(len(parsed_data)):
+        difference = int(parsed_data.iloc[row, col1]) - int(parsed_data.iloc[row, col2])
+        goal_dict[parsed_data.index[row]] = abs(difference)
+    return goal_dict
+
+
+goals_dict = get_min_score_difference(football, 4, 5)
+
+print(goals_dict)
+
+
+def get_team(parsed_data, parsed_dict):
+    
+    # make list of absolute value goals.
+    goals_list = []
+    for i in range(len(parsed_data)):
+        a = parsed_dict.get(parsed_data.index[i])
+        goals_list.append(a)
+    
+    team_goals = min(goals_list)
+    print('min difference: ' + str(team_goals))
+    
+    for team in parsed_dict:
+        if parsed_dict[team] == team_goals:
+            print(team)
+
+get_team(football, goals_dict)
+
